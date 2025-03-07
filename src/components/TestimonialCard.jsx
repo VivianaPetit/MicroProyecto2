e6c2b80c86032baa5b11272e2e9ecce96c0e7f6e
Marcela Stallone <chela@MacBook-Air-de-Marcela.local>
2025-03-06 23:04:15 -0400

    correccion testimonios, falta centrar el texto

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -10,18 +10,16 @@ export const TestimonialCard = ({
 }) => {
   return (
     <div className={`flex flex-col m-8 lg:flex-row items-center lg:items-start gap-8 p-4 ${className}`} data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
-      <div className="lg:w-1/2 flex justify-center">
+      <div className="lg:w-1/2 flex flex-col items-center">
         <img className="w-40 h-40 object-cover rounded-full" src={image} alt={name} />
+        <h3 className="text-[20px] font-extrabold text-black text-center mt-4">{name}</h3>
+        <p className="text-[14px] text-black text-center">{career}</p>
+        <time className="block text-[14px] text-black text-center">{date}</time>
       </div>
-      <div className="relative text-2xl lg:w-[600px] w-[560px]" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
-        <h3 className="lg:text-[32px] md:text-[28px] text-[24px] font-extrabold mb-5 text-[#889e19] text-center">{name}</h3>
-        <p className="lg:text-[16px] md:text-[15px] text-[14px] text-center">{career}</p>
-        <blockquote className="mt-5 text-center text-black lg:text-[18px] md:text-[16px] text-[14px]">
+      <div className="relative text-2xl lg:w-[600px] w-[560px] flex items-center justify-center" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">
+        <blockquote className="mt-10 text-center text-black lg:text-[18px] md:text-[16px] text-[14px]">
           {testimonial}
         </blockquote>
-        <time className="block mt-5 text-center text-[#889e19] lg:text-[16px] md:text-[15px] text-[14px]">
-          {date}
-        </time>
       </div>
     </div>
   );
